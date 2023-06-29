76b352b9871b142f7a2a39e6be3320ca4acea569
mauricio-delgado <mauri.delgadom2001@gmail.com>
2023-06-29 12:26:26 -0300

    cambios

--- index.js
+++ index.js
@@ -10,60 +10,67 @@ const clientes = [
 function yaEsCliente() {
   const actualCliente = parseInt(prompt("1-Ya soy cliente 2-Nuevo cliente"));
   if (seleccion === 1) {
-    menu();
+    login();
   } else if (seleccion === 2) {
     newCliente();
   } else {
     alert("Opción inválida");
+    yaEsCliente();
   }
 }
 yaEsCliente();
-{
-  function menu() {
-    const seleccion = parseInt(
-      prompt("1- Ver préstamos 2-Nuevo préstamo 3-Salir")
-    );
-    if (seleccion === 1) {
-      verPrestamo();
-    } else if (seleccion === 2) {
-      nuevoPrestamo();
-    } else {
-      yaEsCliente();
-    }
+
+function login() {
+  const cliente = prompt("Ingrese su nombre");
+  const cedula = prompt("Ingrese su cedula");
+  const clienteExiste = clientes.find((c) => c.cedula === cliente);
+  if (clienteExiste()) {
+    menu();
+  } else {
+    alert("Usted no es cliente");
+    yaEsCliente();
+  }
+}
+function menu() {
+  const seleccion = parseInt(
+    prompt("1- Ver préstamos 2-Nuevo préstamo 3-Salir")
+  );
+  if (seleccion === 1) {
+    verPrestamo();
+  } else if (seleccion === 2) {
+    nuevoPrestamo();
+  } else if (seleccion === 3) {
+    return;
+  } else {
     menu();
   }
-  function verPrestamo() {
-    const cliente = prompt("Ingrese su nombre");
-    const cedula = prompt("Ingrese su cedula");
-    const clienteExiste = clientes.find((c) => c.cedula === cliente);
-    alert(
-      ` Bienvendio, ${clienteExiste.nombre}, usted tiene en curso un préstamo de ${clienteExiste.prestamo} a pagar en ${clienteExiste.plazo} cuotas`
-    );
-  }
+}
+function verPrestamo() {
+  alert(
+    ` Bienvendio, ${clienteExiste.nombre}, usted tiene en curso un préstamo de ${clienteExiste.prestamo} a pagar en ${clienteExiste.plazo} cuotas`
+  );
+}
 
-  function nuevoPrestamo() {
-    const clienteNombre = prompt("Ingrese su nombre");
-    const clienteCedula = prompt("Ingrese cédula");
-    const newPrestamo = parseInt(
-      prompt("ingrese el monto de su nuevo préstamo")
-    );
-    const newPlazo = parseInt(prompt("ingrese el plazo en meses"));
-    const clienteExiste = clientes.find((c) => c.cedula === cliente);
-    const estePrestamo = {
-      prestamo: newPrestamo,
-      plazo: newPlazo,
-    };
-    clientes.push(estePrestamo);
-  }
+function nuevoPrestamo() {
+  const clienteNombre = prompt("Ingrese su nombre");
+  const clienteCedula = prompt("Ingrese cédula");
+  const newPrestamo = parseInt(prompt("ingrese el monto de su nuevo préstamo"));
+  const newPlazo = parseInt(prompt("ingrese el plazo en meses"));
+  const clienteExiste = clientes.find((c) => c.cedula === cliente);
+  const estePrestamo = {
+    prestamo: newPrestamo,
+    plazo: newPlazo,
+  };
+  clientes.push(estePrestamo);
+}
 
-  function newCliente() {
-    const clienteNombre = prompt("Ingrese su nombre");
-    const clienteCedula = prompt("Ingrese cédula");
-    const nuevoCliente = {
-      nombre: clienteNombre,
-      cedula: clienteCedula,
-    };
-    clientes.push(nuevoCliente);
-    menu();
-  }
+function newCliente() {
+  const clienteNombre = prompt("Ingrese su nombre");
+  const clienteCedula = prompt("Ingrese cédula");
+  const nuevoCliente = {
+    nombre: clienteNombre,
+    cedula: clienteCedula,
+  };
+  clientes.push(nuevoCliente);
+  menu();
 }
