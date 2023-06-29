c33cb998750815401a0d655f1cac396c32f9fc53
mauricio-delgado <mauri.delgadom2001@gmail.com>
2023-06-29 11:51:47 -0300

    nuevas funciones y optimizacion

--- index.js
+++ index.js
@@ -1,129 +1,69 @@
-let clientes = [];
-
-let nombreCliente = prompt("Ingrese su nombre");
-let cedula = prompt("Ingrese su cédula");
-
-function yaEsCliente(cedula) {
-  for (let i = 0; i < clientes.length; i++) {
-    if (clientes[i].cedula === cedula) {
-      return true;
-    }
-  }
-  return false;
-}
-
-let nuevoCliente = {};
-if (yaEsCliente(cedula)) {
-  alert("El cliente ya está registrado");
-} else {
-  nuevoCliente = {
-    nombre: nombreCliente,
-    cedula: cedula,
-    prestamoEnCurso: false,
-    dineroDebido: 0,
-  };
-}
-clientes.push(nuevoCliente);
-console.log(clientes);
-
-function tieneDeuda(cedula) {
-  for (let i = 0; clientes.length; i++) {
-    if (clientes[i].cedula === cedula && clientes[i].dineroDebido >= 30000) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-if (tieneDeuda(cedula)) {
-  alert("Usted no puede solicitar préstamos debido a que tiene uno en curso");
-} else {
-  let financiacion = parseFloat(
-    prompt("Ingrese su monto deseado para el préstamo")
-  );
-  if (financiacion > 100000) {
-    alert(
-      "Nuestro préstamo máximo online es de $100.000. Prueba en alguna de nuestras sucursales!"
-    );
+const clientes = [
+  {
+    nombre: "Mauricio Delgado",
+    mail: "mauridelgado@mauridelgado.com",
+    cedula: 11011010,
+    prestamo: 50000,
+    plazo: 12,
+  },
+];
+function yaEsCliente() {
+  const actualCliente = parseInt(prompt("1-Ya soy cliente 2-Nuevo cliente"));
+  if (seleccion === 1) {
+    menu();
+  } else if (seleccion === 2) {
+    newCliente();
   } else {
-    let numeroCuotas = parseInt(prompt("Ingrese número de cuotas"));
-    function pagoFinal(finaciacion, recargo) {
-      pagoAlFinal = financiacion * recargo;
-      return pagoAlFinal;
+    alert("Opción inválida");
+  }
+}
+yaEsCliente();
+{
+  function menu() {
+    const seleccion = parseInt(
+      prompt("1- Ver préstamos 2-Nuevo préstamo 3-Salir")
+    );
+    if (seleccion === 1) {
+      verPrestamo();
+    } else if (seleccion === 2) {
+      nuevoPrestamo();
+    } else {
+      yaEsCliente();
     }
-    function precioCuotas(pagoFinal, numeroCuotas) {
-      precioPorCuotas = pagoFinal / numeroCuotas;
-      return precioPorCuotas;
-    }
-    while (numeroCuotas > 24) {
-      alert("El número máximo de cuotas es 24");
-      numeroCuotas = parseInt(prompt("Ingrese número de cuotas"));
-    }
-    if (numeroCuotas === 1) {
-      alert(
-        "Hola " +
-          nombreCliente +
-          "! Su préstamo es de: $" +
-          financiacion +
-          " y usted lo pagará en 1 cuota de $" +
-          financiacion
-      );
-    } else if (numeroCuotas <= 3) {
-      const hastaTres = pagoFinal(financiacion, 1.08);
-      const precioDeTres = precioCuotas(hastaTres, numeroCuotas);
-      alert(
-        "Hola " +
-          nombreCliente +
-          "! Su préstamo es de: $" +
-          financiacion +
-          " y usted lo pagará en " +
-          numeroCuotas +
-          " cuotas de $" +
-          precioDeTres
-      );
-    } else if (numeroCuotas <= 6) {
-      const hastaSeis = pagoFinal(financiacion, 1.12);
-      const precioHastaSeis = precioCuotas(hastaSeis, numeroCuotas);
-      alert(
-        "Hola " +
-          nombreCliente +
-          "! Su préstamo es de: $" +
-          financiacion +
-          " y usted lo pagará en " +
-          numeroCuotas +
-          " cuotas de " +
-          precioHastaSeis
-      );
-    } else if (numeroCuotas <= 12) {
-      const hastaDoce = pagoFinal(financiacion, 1.18);
-      const precioHastaDoce = precioCuotas(hastaDoce, numeroCuotas);
-      alert(
-        "Hola " +
-          nombreCliente +
-          "! Su préstamo es de: $" +
-          financiacion +
-          " y usted lo pagará en " +
-          numeroCuotas +
-          " cuotas de " +
-          precioHastaDoce
-      );
-    } else if (numeroCuotas <= 24) {
-      const hastaVeinticuatro = pagoFinal(financiacion, 1.22);
-      const precioHastaVeinticuatro = precioCuotas(
-        hastaVeinticuatro,
-        numeroCuotas
-      );
-      alert(
-        "Hola " +
-          nombreCliente +
-          "! Su préstamo es de: $" +
-          financiacion +
-          " y usted lo pagará en " +
-          numeroCuotas +
-          " cuotas de " +
-          precioHastaVeinticuatro
-      );
-    }
+    menu();
+  }
+  function verPrestamo() {
+    const cliente = prompt("Ingrese su nombre");
+    const cedula = prompt("Ingrese su cedula");
+    const clienteExiste = clientes.find((c) => c.cedula === cliente);
+    alert(
+      ` Bienvendio, ${clienteExiste.nombre}, usted tiene en curso un préstamo de ${clienteExiste.prestamo} a pagar en ${clienteExiste.plazo} cuotas`
+    );
+  }
+
+  function nuevoPrestamo() {
+    const clienteNombre = prompt("Ingrese su nombre");
+    const clienteCedula = prompt("Ingrese cédula");
+    const newPrestamo = parseInt(
+      prompt("ingrese el monto de su nuevo préstamo")
+    );
+    const newPlazo = parseInt(prompt("ingrese el plazo en meses"));
+    const clienteExiste = clientes.find((c) => c.cedula === cliente);
+    const estePrestamo = {
+      prestamo: newPrestamo,
+      plazo: newPlazo,
+    };
+    clientes.push(estePrestamo);
+  }
+
+  function newCliente() {
+    const clienteNombre = prompt("Ingrese su nombre");
+    const clienteCedula = prompt("Ingrese cédula");
+    const nuevoCliente = {
+      nombre: clienteNombre,
+      cedula: clienteCedula,
+    };
+    clientes.push(nuevoCliente);
+    menu();
   }
 }
